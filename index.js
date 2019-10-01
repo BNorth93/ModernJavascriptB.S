512f0b2a80f486fd1416572e1cf1c87cc4586099
BNorth93 <55454947+BNorth93@users.noreply.github.com>
2019-10-01 18:11:34 +0100

    Moving Javascript to external file

---
+++ index.js
@@ -0,0 +1,5 @@
+const things = ['Balvenie Doublewood','Glenlivit Founder\'s Reserve', 'Oban Little Bay'];
+    const mainView = document.querySelector('#primaryView');
+       mainView.innerHTML = `
+       <h2>Text</h2>
+    `;
