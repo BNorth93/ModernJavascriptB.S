079c5ab41d7f59b21ada166bc309f591752aab53
BNorth93 <55454947+BNorth93@users.noreply.github.com>
2019-10-08 19:23:52 +0100

    Classifications

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import { render } from './renderer.js';
 import * as data from './data.json';
 
+function getThingTemplate(thing){
+  return `<li>${thing.name} - ${thing.description}</li>`;
+}
+
 console.log(data.results);
 const things = data.results;
-let content = '';
+let content = '<ul>';
 for(let i=0; i < things.length; i+=1) {
   console.log(things[i]);
-  content +=things[i].name; 
+  content += getThingTemplate(things[i]); 
 }
+content += '</ul>';
 console.log(content);
 
   render(content);
