59613bfb9d65e2a64aff813cf15ebc6593446894
BNorth93 <55454947+BNorth93@users.noreply.github.com>
2019-10-08 17:50:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { render } from './renderer.js';
+import * as data from './data.json';
+
+console.log(data.results);
 
 const things = ['Character 1','Character 2', 'Character 3'];
   render('<h2>The PC\'s</h2>')
